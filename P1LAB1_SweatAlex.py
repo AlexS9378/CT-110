b1908f03327bdd7f88b9567fc90eddc6f6d12cb5
AlexS9378 <sweata9378@student.faytechcc.edu>
2025-04-01 15:35:55 -0700

    Add files via upload

---
+++ P1LAB1_SweatAlex.py
@@ -0,0 +1,13 @@
+#Alex Sweat
+#04/01/2025
+#P1LAB1
+#Using pythons input and print functions to create a program.
+
+# Prompt Enter First Name
+first_name = input("Please enter first name:")
+
+#Prompt Enter Last Name
+last_name = input("Please enter last name:")
+
+#Greeting Message
+print(f"Hello, {first_name} {last_name}! Welcome to CTI-110.")
